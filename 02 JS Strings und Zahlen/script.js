6b76e737a604305957ca4f28a3862f765079607b
maurice <maurice@htl.do>
2024-01-11 11:20:29 +0100

    02 JS Strings und Zahlen finished

---
+++ 02 JS Strings und Zahlen/script.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+let name = "Maurice"
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
