e4c766b84db203c7c54d9c0af1600faa283d7a4c
maurice <maurice@htl.do>
2024-01-25 11:11:46 +0100

    05 If Bedingung

---
+++ JS_Basic/01 Variablen und Operatoren/05 IF bedingungen/script.js
@@ -0,0 +1,41 @@
+
+let number1 = 28;
+if (number1 < 25) {
+    console.log(number1);
+} else if(number1<50){
+    console.log("kleiner 50 " + number1);
+} else {
+    console.log("else");
+}
+
+let number = 51;
+
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+
+if(number<20) {
+    console.log("Mini")
+} else if(number<50) {
+    console.log("Medium")
+}else{
+    console.log("Large")
+}
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+
+let number2 = 90;
+
+if(number2<=7) {
+    console.log("Mini")
+}else if(number2<90) {
+    console.log("Medium")
+}else{
+    console.log("Large")
+}
